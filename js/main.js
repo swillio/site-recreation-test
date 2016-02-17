db957a29c67322692ea5361b62e04f1bcd006577
SeanWilliamson707 <SeanWilliamson707@gmail.com>
2016-02-17 14:41:38 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+  Sean to-do:
+  + Swap image out to white whenever a dark background occurs. Do the inverse for white images.
+  + Make the homepage descriptions non-static. That is, create a JS function that rotates images and descriptions.
+*/
+
+var logoSwapIOrg = {
+  "light": "https://s0.wp.com/wp-content/themes/vip/fb-internetorg/_static/web/assets/media/images/logo-fallback-white.png",
+  "dark": "https://s0.wp.com/wp-content/themes/vip/fb-internetorg/_static/web/assets/media/images/logo-fallback.png"
+}
+
+var imagesIOrg = {
+  "img1": "https://fbinternetorg.files.wordpress.com/2015/10/js_20141215_6457-960x12001.jpg?w=960&h=1200&crop=1",
+  "img2": "https://fbinternetorg.files.wordpress.com/2015/10/js_20141220_7019-960x12001.jpg?w=960&h=1200&crop=1",
+  "img3": "https://fbinternetorg.files.wordpress.com/2015/10/kenya_12-960x6401.jpg?w=960&h=640&crop=1",
+  "img4": "https://fbinternetorg.files.wordpress.com/2015/10/mission1.jpg?w=640&h=960&crop=1"
+};
---
+++ wp-content/themes/internet.org/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title></title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link rel="apple-touch-icon" href="apple-touch-icon.png">
+      <!-- Place favicon.ico in the root directory -->
+
+      <link rel="stylesheet" href="../../../css/normalize.css">
+      <link rel="stylesheet" href="../../../css/main.css">
+      <link rel="stylesheet" href="../../../css/home.css">
+      <script src="../../../js/vendor/modernizr-2.8.3.min.js"></script>
+      <!-- <3 Fonts & Icons <3 -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+      <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+
+  </head>
+  <body>
+
+    <php? echo "hello"; ?>
+
+  <!--[if lt IE 8]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <section class="container">
+    <div class="left-half">
+      <article>
+        <h1 id="title">Connecting the world</h1>
+        <p>means the whole world, not just some of us</p>
+      </article>
+    </div>
+    <div class="right-half"></div>
+  </section>
+
+  <style>
+      #outermost {
+          background: #eee;
+          width: 500px;
+          height: 500px;
+      }
+
+      #outermost {
+          background: #eee;
+          width: 500px;
+          height: 500px;
+      }
+  </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+    <script src="../../js/plugins.js"></script>
+    <script src="../../js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+
+    </body>
+  </html>
